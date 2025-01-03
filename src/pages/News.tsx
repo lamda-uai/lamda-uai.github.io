f8a16026ee636a5e9b400288fbe9307b629a1875
smorenoa <sebastian.moreno.araya@gmail.com>
2025-01-03 10:48:55 -0300

    Update News.tsx

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -18,10 +18,10 @@ export function News(): ReactElement {
           <li>
             <b>Jan. 2025: </b> Sofía Álvarez has started her internship in ENTEL, Chile.
           </li>
+	<li></li>
 	   <li>
             <b>Dic. 2024: </b> Agustín Ghent attended to the Latin American Summer School on Robotics (LACORO), Rancagua Chile.
           </li>
-		
           <li>
             <b>Dec. 2024: </b> Denis Berroeta has successfully defended his Master of Science in Data Science thesis, Congratulations!!!
           </li>
@@ -59,7 +59,7 @@ export function News(): ReactElement {
             <b>April 2024: </b> <a href="https://www.sciencedirect.com/science/article/pii/S2352711024001031">Paper accepted</a> in the journal <a href="https://www.sciencedirect.com/journal/softwarex">SoftwareX</a>  (WOS-Q2). Congrats to Heredia.
           </li>          		
           <li>
-            <b>April 2024: </b> <a href="https://doi.org/10.3390/math12081255">Paper accepted</a> in the journal <a href="https://www.mdpi.com/journal/mathematics">Mathematics)</a> (WOS-Q1).
+            <b>April 2024: </b> <a href="https://doi.org/10.3390/math12081255">Paper accepted</a> in the journal <a href="https://www.mdpi.com/journal/mathematics">Mathematics</a> (WOS-Q1).
           </li>          				
           <li>
             <b>March 2024: </b> We give a warm welcome to Agustín Ghent, who has started the Master of Science in Data Science.
@@ -79,6 +79,9 @@ export function News(): ReactElement {
           <li>
             <b>Nov. 2023: </b> Cristobal Nazar has successfully defended his Master of Science in Engineering, thesis, Congratulations!!!
           </li> 
+          <li>
+            <b>Sept. 2023: </b> <a href="https://doi.org/10.1007/s10339-023-01160-2">Paper published</a> in the journal <a href="https://link.springer.com/journal/10339">Cognitive Processing</a> (WOS-Q4).
+          </li> 						
 	  <li>
             <b>June 2023: </b> Abstract accepted at the International Conference on Data Science <a href="https://icds2023.cl">ICDS 2023</a>. Congrats to Bravo-Diaz.
           </li> 
@@ -92,6 +95,9 @@ export function News(): ReactElement {
             <b>Dec. 2022: </b> We won the initiation FONDECYT fund research, by ANID, Chile.
           </li> 		
           <li>
+            <b>Dec. 2022: </b> <a href="https://doi.org/10.3758/s13428-022-02030-z">Paper published</a> in the journal <a href="https://link.springer.com/journal/13428">Behavior Reserach Methods</a> (WOS-Q1).
+          </li> 						
+          <li>
             <b>Sept. 2022: </b> Bárbara Salazar has successfully defended his Master of Science in Engineering thesis, Congratulations!!!
           </li> 
 	  <li>
@@ -101,7 +107,7 @@ export function News(): ReactElement {
             <b>June 2022: </b> Rodrigo Martinez has successfully defended his Master of Science in Engineering thesis, Congratulations!!!
           </li> 
           <li>
-            <b>Jan. 2022: </b> Valentina Martinez has successfully defended his Master of Science in Engineering thesis, Congratulations!!!
+            <b>Jan. 2022: </b> Sebastián Moreno is an Associate Professor at Universidad Adolfo Ibáñez!!!
           </li> 		
           <li>
             <b>Jan. 2022: </b> Valentina Martinez has successfully defended his Master of Science in Engineering thesis, Congratulations!!!
