3e42acbeacc89c017c1f3fc1dfdfb5ba8a2cf797
smorenoa <sebastian.moreno.araya@gmail.com>
2025-01-07 23:14:47 -0300

    Update People.tsx

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -55,15 +55,33 @@ export function People(): ReactElement {
               <div className="portrait">
                 <img
                   alt=""
-                  src="images/DiegoOpazo.jpg"
+                  src="images/BarbaraSalazar.jpg"
                   className="portrait-round"
                 />
               </div>
-            <h3>Diego Opazo</h3>
-            <p>MSc Graduate (2021)</p>
-            <h5>Triada Solutions</h5>
-            <h5>Data Scientist</h5>
-          </div>
+            <h3>
+              <a href="https://www.linkedin.com/in/barbsp/">Bárbara Salazar</a>
+            </h3>
+            <p>Master of Science in Engineering (2022)</p>
+            <h5>Akloe</h5>
+            <h5>Consultant</h5>
+          </div>    
+          
+          <div className="banner-bottom-left">
+              <div className="portrait">
+                <img
+                  alt=""
+                  src="images/RodrigoMartinez.jpg"
+                  className="portrait-round"
+                />
+              </div>
+            <h3>
+              <a href="https://www.linkedin.com/in/rodrigo-martínez-araya-53a09b16b/">Rodrigo Martinez</a>
+            </h3>
+            <p>Master of Science in Engineering (2022)</p>
+            <h5>Walmart</h5>
+            <h5>Full Stack Developer</h5>
+          </div>          
           
           <div className="banner-bottom-left">
               <div className="portrait">
@@ -73,10 +91,28 @@ export function People(): ReactElement {
                   className="portrait-round"
                 />
               </div>
-            <h3>Valentina Martinez</h3>
-            <p>Master of Science(2022)</p>
+            <h3>
+              <a href="https://www.linkedin.com/in/valentina-martinez-pastene/">Valentina Martinez</a>
+            </h3>
+            <p>Master of Science in Engineering (2022)</p>
             <h5>Cotalker</h5>
             <h5>Product Lead</h5>
+          </div>
+          
+          <div className="banner-bottom-left">
+              <div className="portrait">
+                <img
+                  alt=""
+                  src="images/DiegoOpazo.jpg"
+                  className="portrait-round"
+                />
+              </div>
+            <h3>
+              <a href="https://www.linkedin.com/in/diego-opazo-inarejo/">Diego Opazo</a>
+            </h3>
+            <p>Master of Science in Engineering (2021)</p>
+            <h5>Triada Solutions</h5>
+            <h5>Data Scientist</h5>
           </div>
           
           <div className="banner-bottom-left">
@@ -87,7 +123,9 @@ export function People(): ReactElement {
                   className="portrait-round"
                 />
               </div>
-            <h3>Daniela Miranda</h3>
+            <h3>
+              <a href="https://www.linkedin.com/in/dalejandramb/">Daniela Miranda</a>
+            </h3>
             <p>Bachelor's in Engineering (2021)</p>
             <h5>Cotalker</h5>
             <h5>Customer Success Lead</h5>
@@ -101,7 +139,9 @@ export function People(): ReactElement {
                   className="portrait-round"
                 />
               </div>
-            <h3>Martín Pinto</h3>
+            <h3>
+              <a href="https://www.linkedin.com/in/martin-pinto-stevens/">Martín Pinto</a>
+            </h3>
             <p>Bachelor's in Engineering (2021)</p>
             <h5>Coderhub</h5>
             <h5>Product Owner</h5>
