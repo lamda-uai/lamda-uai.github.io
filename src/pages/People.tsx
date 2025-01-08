4bd243d4c20bcfc54c1f7224499ecc3f8e58114b
Agustín Ghent <agustinghent@gmail.com>
2025-01-08 14:28:51 -0300

    bugfixes with format

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -23,7 +23,6 @@ export function People(): ReactElement {
             <p>Associate Professor</p>
           </div>
 
-
           {/*  Students  */}
 
           <div className="clearfix"> </div>
@@ -45,203 +44,225 @@ export function People(): ReactElement {
             <p>MSc Student</p>
           </div>
 
-          
-
           {/* Former Members */}
           <div className="clearfix"> </div>
           <h2>Former Members</h2>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/DiegoCorvalan.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/DiegoCorvalan.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/diego-corvalán-morbiducci-236642278/">Diego Corvalán</a>
             </h3>
             <p>MSc. in Engineering (2024)</p>
-            <h5>???</h5>
-            <h5>Data Analyst</h5>
-          </div> 
+            <div className="details">
+              <h5>???</h5>
+              <h5>Data Analyst</h5>
+            </div>
+          </div>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/NinneteOlhabe.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/NinneteOlhabe.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/ninnette-olhabé-ferrat-3a1545303/">Ninnete Olhabe</a>
             </h3>
             <p>MIIIO (2024)</p>
-            <h5>Form Design</h5>
-            <h5>Category manager</h5>
-          </div> 
-          
+            <div className="details">
+              <h5>Form Design</h5>
+              <h5>Category manager</h5>
+            </div>
+          </div>
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/NicolasMartinez.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/NicolasMartinez.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/nicolás-martínez-alfaro-396459148/">Nicolás Martinez</a>
             </h3>
             <p>MSc. in Data Science (2023)</p>
-            <h5>Conomy</h5>
-            <h5>Software Engineering</h5>
-          </div>    
-          
+            <div className="details">
+              <h5>Conomy</h5>
+              <h5>Software Engineering</h5>
+            </div>
+          </div>
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/CristobalHeredia.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/CristobalHeredia.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/crisheredia/">Cristobal Heredia</a>
             </h3>
             <p>MSc. in Data Science (2023)</p>
-            <h5>University of South Florida</h5>
-            <h5>PhD. Student</h5>
-          </div>    
+            <div className="details">
+              <h5>University of South Florida</h5>
+              <h5>PhD. Student</h5>
+            </div>
+          </div>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/CristobalNazar.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/CristobalNazar.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/cristobal-nazar/">Cristobal Nazar</a>
             </h3>
             <p>MSc. in Engineering (2023)</p>
-            <h5>Huawei</h5>
-            <h5>GTM coordinator</h5>
-          </div>    
+            <div className="details">
+              <h5>Huawei</h5>
+              <h5>GTM coordinator</h5>
+            </div>
+          </div>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/BarbaraSalazar.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/BarbaraSalazar.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/barbsp/">Bárbara Salazar</a>
-            </h3> 
+            </h3>
             <p>MSc. in Engineering (2022)</p>
-            <h5>Akloe</h5>
-            <h5>Consultant</h5>
-          </div>    
-          
+            <div className="details">
+              <h5>Akloe</h5>
+              <h5>Consultant</h5>
+            </div>
+          </div>
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/RodrigoMartinez.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/RodrigoMartinez.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/rodrigo-martínez-araya-53a09b16b/">Rodrigo Martinez</a>
             </h3>
             <p>MSc. in Engineering (2022)</p>
-            <h5>Walmart</h5>
-            <h5>Full Stack Developer</h5>
-          </div>          
-          
+            <div className="details">
+              <h5>Walmart</h5>
+              <h5>Full Stack Developer</h5>
+            </div>
+          </div>
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/ValentinaMartinez.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/ValentinaMartinez.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/valentina-martinez-pastene/">Valentina Martinez</a>
             </h3>
             <p>MSc. in Engineering (2022)</p>
-            <h5>Cotalker</h5>
-            <h5>Product Lead</h5>
+            <div className="details">
+              <h5>Cotalker</h5>
+              <h5>Product Lead</h5>
+            </div>
           </div>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/MatiasCoustasse.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/MatiasCoustasse.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/matias-coustasse-mackenzie/">Matias Coustasse</a>
             </h3>
             <p>MIIIO (2021)</p>
-            <h5>Falabella</h5>
-            <h5>Data Scientist</h5>
-          </div>           
-          
+            <div className="details">
+              <h5>Falabella</h5>
+              <h5>Data Scientist</h5>
+            </div>
+          </div>
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/DiegoOpazo.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/DiegoOpazo.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/diego-opazo-inarejo/">Diego Opazo</a>
             </h3>
             <p>MSc. in Engineering (2021)</p>
-            <h5>Triada Solutions</h5>
-            <h5>Data Scientist</h5>
+            <div className="details">
+              <h5>Triada Solutions</h5>
+              <h5>Data Scientist</h5>
+            </div>
           </div>
-          
+
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/DanielaMiranda.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/DanielaMiranda.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/dalejandramb/">Daniela Miranda</a>
             </h3>
             <p>Bachelor's in Engineering (2021)</p>
-            <h5>Cotalker</h5>
-            <h5>Customer Success Lead</h5>
+            <div className="details">
+              <h5>Cotalker</h5>
+              <h5>Customer Success Lead</h5>
+            </div>
           </div>
 
           <div className="banner-bottom-left">
-              <div className="portrait">
-                <img
-                  alt=""
-                  src="images/MartinPinto.jpeg"
-                  className="portrait-round"
-                />
-              </div>
+            <div className="portrait">
+              <img
+                alt=""
+                src="images/MartinPinto.jpeg"
+                className="portrait-round"
+              />
+            </div>
             <h3>
               <a href="https://www.linkedin.com/in/martin-pinto-stevens/">Martín Pinto</a>
             </h3>
             <p>Bachelor's in Engineering (2021)</p>
-            <h5>Coderhub</h5>
-            <h5>Product Owner</h5>
-          </div>          
+            <div className="details">
+              <h5>Coderhub</h5>
+              <h5>Product Owner</h5>
+            </div>
+          </div>
           <div className="clearfix"> </div>
         </div>
       </div>
